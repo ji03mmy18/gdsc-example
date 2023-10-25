a431c2e5f0c319123046909258856087a47ff75e
YoMin <aaaa1234567891016@gmail.com>
2023-10-25 20:28:51 +0800

    test: 撰寫main.py的測試

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ print("Hello World!")
 
 student = ['YoMin', 'Cindy', 'John', 'Kent', 'Charles']
 
+def test():
+    return "Program running"
+
 for s in student:
     print(f"Hi {s}")
+
+print(test())
